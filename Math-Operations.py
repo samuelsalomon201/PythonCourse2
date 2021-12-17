d1b592b1d95f5192347df189f4c60fad3884bfaf
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-12-17 16:08:01 -0500

    Added more .py files

---
+++ Math-Operations.py
@@ -0,0 +1,15 @@
+num1 = 10
+num2 = 2.5
+print(type(num1))
+print(type(num2))
+print(1+2)
+print(num1 + num2)
+print(2-1)
+print(5 / 2)
+print(5 // 2)
+print(4 * 2)
+print(4 ** 2)
+print(5 % 2)
+print(4 < 5)
+print(5 < 4)
+#And a bunch more
