41daed2897c5787a1bb1c2ed5845d648bbb28d9a
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-12-18 11:42:41 -0500

    Added more .py files

---
+++ Lists-Introduction.py
@@ -0,0 +1,13 @@
+list1 = []
+print(type(list1))
+list1 = ["Cisco", "Juniper", "Avaya", 10, 10.5, -11]
+
+print(len(list1))
+print(list1[0])
+print(list1[1])
+print(list1[-1])
+print(list1[-2])
+print(list1)
+print(list1[2])
+list1[2] = "HP"
+print(list1)
