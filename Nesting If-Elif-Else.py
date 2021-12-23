433ff9e25ba3e67e40e83c8f1d3c77eace2764e2
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-12-23 14:56:48 -0500

    Added more .py files

---
+++ Nesting If-Elif-Else.py
@@ -0,0 +1,13 @@
+x = 1
+
+while x <= 10:
+    z = 5
+    x += 1
+    while z <= 10:
+        print(z)
+        print("[ $ ]")
+        z += 1
+# for number in range(10):
+#     if 5 <= number <= 9:
+#         print(number)
+#         print("[ $ ]")
