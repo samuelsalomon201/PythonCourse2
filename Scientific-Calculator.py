b5d67c588c7f4b47e33ddfbc673dd181b15f5395
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-01-23 12:11:04 -0500

    Finished My Sucientific Calculator ඞඞඞඞ

--- Scientific-Calculator.py
+++ Scientific-Calculator.py
@@ -4,7 +4,7 @@ import math
 while True:
     print(
         "\nChoose the math operation.\n\n0 - Addition\n1 - Subtraction\n2 - Multyplication\n3 - Division\n4 - Modulo\n5 - Raising to a power\n6 - Logarithm\n8 - Sine\n9 - Cosine\n10 - Tangent\n")
-    oper = input("\nYour option from the menu: ")
+    oper = input("\nYour option from the menu ඞ: ")
 
     # Addition
     if oper == "0":
