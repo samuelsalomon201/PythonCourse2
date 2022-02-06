304942d8cf7360b4d71fda99ba466b123196b758
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-02-06 11:24:34 -0500

    ADded More

--- Exell-Tasks.py
+++ Exell-Tasks.py
@@ -6,11 +6,32 @@ print(workbook.sheetnames)
 print(workbook.active)
 sheet = workbook['EmployeeData']
 workbook.create_sheet('TestSheet')
-workbook.save("E:\Employees.xlsx")
+workbook.save("EmployeeData")
 
 sheet = workbook['TestSheet']
 workbook.remove(sheet)
 
-workbook.save("E:\Employees.xlsx")
+workbook.save("EmployeeData")
 
-workbook.close()
+sheet = workbook['EmployeeData']
+
+print(sheet.title)
+
+print(dir(sheet))
+
+
+print(sheet.active_cell)
+
+print(sheet.dimensions)
+
+print(sheet.max_row)
+
+print(sheet.max_column)
+
+print(sheet.sheet_properties)
+
+print(sheet.sheet_view)
+
+
+for i in sheet.values:
+    print(i)
