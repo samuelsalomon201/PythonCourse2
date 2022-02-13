83d816471119e58e388907076fd84272a05e912b
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-02-13 12:04:54 -0500

    I Got A BOO BOO

---
+++ Strings-Slices.py
@@ -0,0 +1,9 @@
+myminecraftworld = "Survival, Cheats ON, Version 1.16.5, Difficulty Normal, Mods ON"
+
+print(myminecraftworld[10:19])
+print(myminecraftworld[15:25])
+print(myminecraftworld[50:63])
+print(myminecraftworld[21:38])
+print(myminecraftworld[:-31])
+print(myminecraftworld[-31])
+print(len(myminecraftworld))
---
+++ Strings-Slicing-Using-A-Step.py
@@ -0,0 +1,8 @@
+myminecraftmods = "Ripples Of The Past, MrCrayFish Furniture Mod"
+print(myminecraftmods[0])
+print(myminecraftmods[1])
+print(myminecraftmods[9])
+print(myminecraftmods[::2])
+print(myminecraftmods[1::2])
+print(myminecraftmods[1:7:2])
+print(len(myminecraftmods))
