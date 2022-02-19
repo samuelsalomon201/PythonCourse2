3063c967626cc9ef0929f38823c6959e614c9cfa
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-02-19 14:07:32 -0500

    OH NO I MADE A HUMONGOUS BONER

---
+++ Tuples-Introduction-Noob-Level.py
@@ -0,0 +1,23 @@
+my_minecraft_pet_axolotl = ()
+print(type(my_minecraft_pet_axolotl))
+my_minecraft_pet_axolotl = 10
+print(type(my_minecraft_pet_axolotl))
+my_minecraft_pet_axolotl_health_levels = (1, 2, 3, 4, 5)
+print(my_minecraft_pet_axolotl_health_levels)
+print(my_minecraft_pet_axolotl_health_levels[0])
+print(my_minecraft_pet_axolotl_health_levels[1])
+print(my_minecraft_pet_axolotl_health_levels[-1])
+print(my_minecraft_pet_axolotl_health_levels[-2])
+# print(my_minecraft_pet_axolotl_health_levels[1] = 10)
+# del my_minecraft_pet_axolotl_health_levels[1]
+
+minecraft_mob_drops = ("Creeper", "Pig", "Baby Zombie")
+(Gun_Powder, Raw_Pork_Chop, Zombie_Flesh) = minecraft_mob_drops
+
+print(Gun_Powder)
+print(Raw_Pork_Chop)
+print(Zombie_Flesh)
+
+# minecraft_most_useful_ores = ("Iron", "Red Stone Dust", "Netherite", "Coal", "Quarts")
+# (Gold, Glow_Stone_Dust, Diamonds, Wood) = minecraft_most_useful_ores
+# This Code Above Gives A Big Boner
