cdf931832609ed7194e5ed5bed2fefeea6fa1c25
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-02-21 10:18:36 -0500

    Hewo
    Added 2 More .py files make sure to not check them :)

---
+++ Loops-For-For-Else-Pro-X-Noob-Level.py
@@ -0,0 +1,6 @@
+for element in night_mobs:
+    print(element)
+else:
+    print("The End Of The List Has Been Reached")
+
+# This Code Has Been Copied And Pasted To "Loops-For-Else-Pro-X-Noob-Level-Extra-File.py".
