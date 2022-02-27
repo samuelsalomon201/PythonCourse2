210996bff97f1f76c53410ca0b261e98346743b8
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-02-27 12:22:08 -0500

    YAY FINISHED SECTION 11 NICE NICE :))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))

---
+++ Fixing-Exeptions-Rokie-Level.py
@@ -0,0 +1,18 @@
+# from sys import abcdef
+#
+# import abcdef
+
+Creeper_boom_range = 10
+Pig_carrot_smelling_range = 0
+
+# c1 = Creeper_boom_range / Pig_carrot_smelling_range
+# print(c1)
+
+# p2 = Creeper_boom_range + "HELL-O WIP"
+
+text = "PYTHON"
+# c4 = text[20]
+
+while True:
+    print("Hi")
+    input("")
---
+++ Fixing-Syntax-Errors-Rokie-Level.py
@@ -0,0 +1,10 @@
+my_explosive_pig = [1, 2, 3]
+
+print(my_explosive_pig)
+
+for pp in my_explosive_pig:
+    print(pp * 10)
+
+my_unexplosive_creeper = 10
+
+float(my_unexplosive_creeper)
