42ad5336110c33b39b6263c4cfd19afa29bd4663
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-03-14 07:25:42 -0400

    LET IT FART LET IT FART
    (Cuz HE MADE A BIG BONER)

--- Fixing-Exeptions.py
+++ Fixing-Exeptions.py
@@ -1,17 +1,18 @@
-#from sys import abcdef
+# from sys import abcdef
 
-#import abcdef
+# import abcdef
 
 var1 = 10
 var2 = 0
 
-#r1 = var1 / var2
-#print(r1)
+# r1 = var1 / var2
+# print(r1)
 
-#r2 = var1 + "hello"
+# r2 = var1 + "hello"
 
 text = "Python"
-#r4 = text[20]
+# r4 = text[20]
 
 while True:
-    print("*")
+    print("---> [ $ ] <---")
+    input("INSERT CASH NOW")
