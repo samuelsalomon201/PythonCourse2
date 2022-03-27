87fcd45fab315d2648e66863a2fe1563a3fa60e8
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-03-27 14:43:53 -0400

    Update Regex_MATCH()_&_SEARCH().py

--- Regex_MATCH()_&_SEARCH().py
+++ Regex_MATCH()_&_SEARCH().py
@@ -3,26 +3,26 @@ JOJOFILES = "Part 1 = Phantom Blood Part 2 = Battle Tendency Part 3 = Stardust C
 
 import re
 
-WAAWAAWAA = re.match("Part 1", JOJOFILES)
-print(WAAWAAWAA)
-print(type(WAAWAAWAA))
+WAAAWAAAWAAA = re.match("Part 1", JOJOFILES)
+print(WAAAWAAAWAAA)
+print(type(WAAAWAAAWAAA))
 
-WAAWAAWAA = re.match("AMOGUS", JOJOFILES)
-print(WAAWAAWAA)
-print(type(WAAWAAWAA))
+WAAAWAAAWAAA = re.match("AMOGUS", JOJOFILES)
+print(WAAAWAAAWAAA)
+print(type(WAAAWAAAWAAA))
 
-WAAWAAWAA = re.match("Part 1", JOJOFILES)
+WAAAWAAAWAAA = re.match("Part 1", JOJOFILES)
 
-print(WAAWAAWAA.group())
+print(WAAAWAAAWAAA.group())
 
-WAAWAAWAA = re.match("part 1", JOJOFILES, re.I)
-print(WAAWAAWAA)
+WAAAWAAAWAAA = re.match("part 1", JOJOFILES, re.I)
+print(WAAAWAAAWAAA)
 
-print(WAAWAAWAA.group())
+print(WAAAWAAAWAAA.group())
 
 JOJOFILES = "Part 1 = Phantom Blood Part 2 = Battle Tendency Part 3 = Stardust Crusaders Part 4 = Diamond Is Unbreakable" \
             " Part 5 = Golden Wing  Part 6 = Jojo Stone Ocean Part 7 = Steel Ball Run Part 8 = Jojolion"
-WAAWAAWAA = re.match("part 1", JOJOFILES, re.I)
-WAAWAAWAA
-print(type(WAAWAAWAA))
+WAAAWAAAWAAA = re.match("part 1", JOJOFILES, re.I)
+WAAAWAAAWAAA
+print(type(WAAAWAAAWAAA))
 # WAAWAAWAA = re.search()
