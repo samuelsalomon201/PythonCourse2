11526404568b709b1fbda469eb02e615003272b8
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2022-03-29 15:48:05 -0400

    :angry:  :anger:
    GGGGGGGGGGGGRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR

---
+++ Decorators(NSFW).py
@@ -0,0 +1,4 @@
+def my_wrapper(candy):
+    def sussy_wrapper():
+        return "Amogus is" + candy() + "sussy!"
+    return sussy_wrapper()
--- Regex_MATCH()_&_SEARCH().py
+++ Regex_MATCH()_&_SEARCH().py
@@ -23,6 +23,9 @@ print(WAAAWAAAWAAA.group())
 JOJOFILES = "Part 1 = Phantom Blood Part 2 = Battle Tendency Part 3 = Stardust Crusaders Part 4 = Diamond Is Unbreakable" \
             " Part 5 = Golden Wing  Part 6 = Jojo Stone Ocean Part 7 = Steel Ball Run Part 8 = Jojolion"
 WAAAWAAAWAAA = re.match("part 1", JOJOFILES, re.I)
-WAAAWAAAWAAA
+print(WAAAWAAAWAAA)
 print(type(WAAAWAAAWAAA))
 # WAAWAAWAA = re.search()
+
+while True:
+    input(":< ")
